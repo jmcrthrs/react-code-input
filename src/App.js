ac1c2569deee23b84caa05849ae96d1af45df369
Jamie <jamie.carruthers@live.com>
2020-07-09 17:04:36 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -12,22 +12,44 @@ export default function App() {
     };
   };
 
+  const onPaste = event => {
+    console.log(event.clipboardData.getData("Text"));
+    const value = event.clipboardData.getData("Text");
+    Array.from(value).forEach((v, index) => {
+      if (index < inputRefs.current.length) {
+        onChange(v, index);
+      }
+    });
+  };
+
   const onChange = (value, index) => {
-    console.log(value, index);
-    // go to the next input
+    inputRefs.current[index].value = value;
+    if (index < inputRefs.current.length - 1) {
+      inputRefs.current[index + 1].focus();
+    }
+  };
+
+  const onFocus = event => {
+    event.target.select();
   };
 
   return (
     <div className="App">
-      {[0, 1, 2, 3, 4].map(index => (
-        <input
-          maxLength="1"
-          ref={setRefAtIndex(index)}
-          onChange={event => {
-            onChange(event.target.value, index);
-          }}
-        />
-      ))}
+      {[0, 1, 2, 3, 4].map(index => {
+        return (
+          <input
+            key={index}
+            autoFocus={index === 0}
+            onFocus={onFocus}
+            maxLength="1"
+            ref={setRefAtIndex(index)}
+            onKeyPress={event => {
+              onChange(event.target.value, index);
+            }}
+            onPaste={onPaste}
+          />
+        );
+      })}
     </div>
   );
 }
